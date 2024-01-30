557ab3681a83246eda3543c506e664e2334c497a
Gaurav Jain <gauravjain0781@gmail.com>
2024-01-30 10:35:44 +0530

    Error Page and Service Page Basic Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Home from './Pages/Home';
+import Service from './Pages/Service';
 import Login from './Pages/Login';
 import Registration from './Pages/Registration';
-import Home from './Pages/Home';
+import Error from './Components/Error';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/services' element={<Service/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/regitration' element={<Registration/>}></Route>
-        {/* <Route path='*' element={<Error/>}></Route> */}
+        <Route path='*' element={<Error/>}></Route>
       </Routes>
     </Router>
   )
---
+++ client/src/Pages/Service.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Navbar from '../Components/Navbar'
+import SignUp from '../Components/SignUp'
+import Footer from '../Components/Footer'
+
+const Service = () => {
+  return (
+    <>
+      <Navbar/>
+      <SignUp/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Service
